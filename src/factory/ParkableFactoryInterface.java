b89e00f11fda6d99655298b16e02f4c43decc26c
zu0feng <815564553@qq.com>
2022-06-26 19:44:28 +0800

    haha

---
+++ src/factory/ParkableFactoryInterface.java
@@ -0,0 +1,8 @@
+package factory;
+
+import exercise_5_4.parkable.Parkable;
+
+public interface ParkableFactoryInterface {
+	Parkable create(String type, String plate, int width, String[] extraRegistrationInfo) throws Exception;
+
+}
